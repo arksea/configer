97af8ce75ebb1b7c52a358019b0f4405529d7092
xiaohaixing <arksea@gmail.com>
2017-07-27 01:55:01 +0800

    应用列表

--- config-server/src/main/angular/config-server/src/app/app.component.ts
+++ config-server/src/main/angular/config-server/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export class Project {
+  id: number;
+  name: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Config Server';
+export class AppComponent implements OnInit {
+
+  projects: object;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('/api/v1/projects').subscribe(data => {
+      this.projects = data;
+    });
+  }
 }
