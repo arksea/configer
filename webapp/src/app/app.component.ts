3f82ec8cb95a4ced267b52f612b0d002fc83b7be
xiaohaixing <arksea@gmail.com>
2018-06-29 21:09:59 +0800

    Json编辑Dialog

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -8,11 +8,10 @@ import {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [{ provide: WidgetRegistry, useClass: DefaultWidgetRegistry }]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
-  constructor(registry: WidgetRegistry) {
+  constructor() {
   }
 }
--- webapp/src/app/config/config-form.component.ts
+++ webapp/src/app/config/config-form.component.ts
@@ -1,17 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Config } from '../configer.model';
 import { ConfigService } from '../config/config.service';
+import {
+  WidgetRegistry,
+  Validator,
+  DefaultWidgetRegistry,
+} from 'ngx-schema-form';
 
 @Component({
   selector: 'app-config-form',
-  templateUrl: './config-form.component.html'
+  templateUrl: './config-form.component.html',
+  providers: [{ provide: WidgetRegistry, useClass: DefaultWidgetRegistry }]
 })
 export class ConfigFormComponent implements OnInit {
   @Input() config: Config;
   opened = false;
   lineCount: number;
-
-  constructor(private service: ConfigService) {
+  schema: any;
+  constructor(private service: ConfigService, private registry: WidgetRegistry) {
   }
 
   ngOnInit(): void {
@@ -21,6 +27,8 @@ export class ConfigFormComponent implements OnInit {
     } else if (this.lineCount < 5) {
       this.lineCount = 5;
     }
+    this.schema = JSON.parse(this.config.doc.metadata);
+    // this.registry.register('clrCheckbox', ClrCheckboxModule);
   }
 
   public open(): void {
